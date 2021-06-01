51b9eee510751474a2b28143457e4d70758330fd
Gtan99 <tandoigiulio9@live.com>
2021-06-01 22:17:54 +0200

    aggiunta register

--- src/app/main/register/register.component.ts
+++ src/app/main/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/apis/api.service';
 
 @Component({
   selector: 'app-register',
@@ -9,18 +12,42 @@ import {FormControl, FormGroup} from "@angular/forms";
 export class RegisterComponent implements OnInit {
 
   registerForm = new FormGroup({
-    firstname: new FormControl(''),
-    surname: new FormControl(''),
-    email: new FormControl(''),
+    username: new FormControl(''),
     password: new FormControl(''),
-    role: new FormControl('1')
+    role: new FormControl('admin')
   });
-  constructor() { }
+
+  constructor(private router: Router,
+    private apiService: ApiService,
+    private snackBar: MatSnackBar) { }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
 
   ngOnInit(): void {
   }
 
-  onSubmit() {
-    console.log(this.registerForm.value)
+  onSubmit(): void{
+    if (this.registerForm.valid){
+      console.log(this.registerForm.value);
+      this.apiService.registerUser(this.registerForm.value).subscribe(
+        (response: any) => {
+          localStorage.setItem('token', response.token)
+          if(response.role == "admin"){
+            this.router.navigate(['/login'])
+          }else {
+            this.router.navigate(['/select-scan'])
+          }
+
+        },
+        error => {
+          console.log(error)
+          this.openSnackBar('Credenziali non corrette.', 'chiudi');
+        }
+      );
+    }
   }
 }
